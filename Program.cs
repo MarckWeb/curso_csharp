5c4f01ac513022aa1cf9315f6b490be54f1c0fc1
MarckWeb <122919106+MarckWeb@users.noreply.github.com>
2023-12-13 16:09:18 +0100

    condiciones en csharp

--- Program.cs
+++ Program.cs
@@ -6,3 +6,45 @@ int roll3 = dice.Next(1, 7);
 
 int total = roll1 + roll2 + roll3;
 Console.WriteLine($"Dice roll: {roll1} + {roll2} + {roll3} = {total}");
+
+
+//"Si dos dados cualesquiera muestran el mismo valor, se obtendrán dos puntos extra por obtener dobles"
+
+if ((roll1 == roll2) || (roll2 == roll3) || (roll1 == roll3))
+{
+   Console.WriteLine("Obtuviste dobles! Bono +2 al total!");
+   total += 2;
+}
+
+//"Si los tres dados que tire muestran el mismo valor, obtendrá seis puntos extra por obtener un triple"
+
+if ((roll1 == roll2) && (roll2 == roll3))
+{
+   Console.WriteLine("Obtuviste triples! +6 bonos extra total");
+   total += 6;
+}
+
+
+if (total >= 15)
+{
+   Console.WriteLine("Ganaste");
+}
+
+if (total < 15)
+{
+   Console.WriteLine("Lo siento, Perdiste");
+}
+
+
+
+//ejemplo, a continuación, se muestra un ejemplo de código sencillo que usa el método string.Contains() para evaluar si una cadena contiene otra.
+
+string message = "existe la palabra max o jose.";
+bool result = message.Contains("max");
+Console.WriteLine(result);
+
+if (message.Contains("jose"))
+{
+   Console.WriteLine("Quien es jose?");
+}
+
