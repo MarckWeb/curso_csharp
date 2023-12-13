11b941f0ab5f10c82cdfa07cd7b3cdf7668d8709
MarckWeb <122919106+MarckWeb@users.noreply.github.com>
2023-12-13 16:24:41 +0100

    condiciones en csharp

--- Program.cs
+++ Program.cs
@@ -12,27 +12,37 @@ Console.WriteLine($"Dice roll: {roll1} + {roll2} + {roll3} = {total}");
 
 if ((roll1 == roll2) || (roll2 == roll3) || (roll1 == roll3))
 {
-   Console.WriteLine("Obtuviste dobles! Bono +2 al total!");
-   total += 2;
+   //"Si los tres dados que tire muestran el mismo valor, obtendrá seis puntos extra por obtener un triple"
+
+   if ((roll1 == roll2) && (roll2 == roll3))
+   {
+      Console.WriteLine("Obtuviste triples! +6 bonos extra total");
+      total += 6;
+   }
+   else
+   {
+      Console.WriteLine("Obtuviste dobles! Bono +2 al total!");
+      total += 2;
+   }
+
 }
 
-//"Si los tres dados que tire muestran el mismo valor, obtendrá seis puntos extra por obtener un triple"
 
-if ((roll1 == roll2) && (roll2 == roll3))
+if (total >= 16)
 {
-   Console.WriteLine("Obtuviste triples! +6 bonos extra total");
-   total += 6;
+   Console.WriteLine("Ganaste un coche nuevo");
 }
-
-
-if (total >= 15)
+else if (total >= 10)
 {
-   Console.WriteLine("Ganaste");
+   Console.WriteLine("Ganaste un protatil nuevo");
 }
-
-if (total < 15)
+else if (total == 7)
 {
-   Console.WriteLine("Lo siento, Perdiste");
+   Console.WriteLine("Ganaste un viaje");
+}
+else
+{
+   Console.WriteLine("Ganaste un gatito");
 }
 
 
