5cb10dffb3121c040aa4a8fb7aa5d1a945c6e590
MarckWeb <122919106+MarckWeb@users.noreply.github.com>
2023-12-13 21:51:47 +0100

    formas de realizar un codigo limpio y entendible

--- P_exercise.cs
+++ P_exercise.cs
@@ -172,3 +172,33 @@ foreach (var orderID in orderIDs)
 {
    Console.WriteLine(orderID);
 }
+
+//mejoramiento de la legibilidad de codigo 
+
+/*
+   This code reverses a message, counts the number of times 
+   a particular character appears, then prints the results
+   to the console window.
+ */
+
+string originalMessage = "The quick brown fox jumps over the lazy dog.";
+
+char[] message = originalMessage.ToCharArray();
+Array.Reverse(message);
+
+int letterCount = 0;
+
+foreach (char letter in message)
+{
+   if (letter == 'o')
+   {
+      letterCount++;
+   }
+}
+
+string newMessage = new String(message);
+
+Console.WriteLine(newMessage);
+Console.WriteLine($"'o' appears {letterCount} times.");
+
+
