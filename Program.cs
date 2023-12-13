44024cd7720cfa07ed4eaf59867f1c2beaf934da
MarckWeb <122919106+MarckWeb@users.noreply.github.com>
2023-12-13 15:41:13 +0100

    first commit

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿
+//Escritura de código que genera tres números aleatorios y los muestre en la salida
+Random dice = new Random();
+
+int roll1 = dice.Next(1, 7);
+int roll2 = dice.Next(1, 7);
+int roll3 = dice.Next(1, 7);
+
+int total = roll1 + roll2 + roll3;
+
+Console.WriteLine($"Dice roll: {roll1} + {roll2} + {roll3} = {total}");
